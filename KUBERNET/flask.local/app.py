6c320d31bc9afcbf973ee2711a1797713cc2dc54
vorlon001 <vorlon001@users.noreply.github.com>
2021-02-15 09:35:17 +0500

    extract docx
    extract data

---
+++ KUBERNET/flask.local/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, Response
+app = Flask(__name__)
+
+counter = 0
+
+@app.route('/')
+def visit():
+    global counter
+    counter = counter + 1
+    result = "Visit number %d\n" % counter
+    return Response(result, mimetype='text/plain')
+
+@app.route('/metrics')
+def metrics():
+    global counter
+    result = "# TYPE hello_world_counter counter\nhello_world_counter %d\n" % counter
+    return Response(result, mimetype='text/plain')
+
+app.run(host="0.0.0.0", port=5000, debug=False);
+
